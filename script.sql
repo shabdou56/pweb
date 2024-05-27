af22948ffb2fe76c456ec05bd62a2272dcfa5c78
shabdou56 <shabdou@gmail.com>
2024-05-27 13:43:52 -0700

    modified:   script.sql

--- script.sql
+++ script.sql
@@ -1,16 +1,16 @@
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(100) NOT NULL,
-    status int  DEFAULT 0,
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etat int  DEFAULT 0,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id INT,
-    category_id INT,
+    cat_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES cat(id)
 );
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(20) NOT NULL,
+    username VARCHAR(25) NOT NULL,
     password VARCHAR(255) NOT NULL,  -- Store hashed passwords, never plain text
 );
 CREATE TABLE cat (
